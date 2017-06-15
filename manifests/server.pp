900674ef103d7932c942d113ce1a2ba848484faa
nick-markowski <nmarkowski@keywcorp.com>
2017-06-15 22:40:05 +0000

    (SIMP-3325) Exposed stunnel::connection parameters
    SIMP-3325 #close
    SIMP-571 #comment exposed in pupmod-simp-rsync

--- manifests/server.pp
+++ manifests/server.pp
@@ -17,6 +17,15 @@
 # @param stunnel_port [Port] The port upon which Stunnel should listen for
 #   connections.
 #
+# @param stunnel_connect [Array[Simplib::Port]] Address and port to which to
+#   **forward** connections
+#
+# @param stunnel_accept [String] Address and port upon which to **accept**
+#   connections
+#
+# @param stunnel_client [Boolean] Indicates that this connection is a client
+#   connection
+#
 # @param listen_address [IPAddress] The IP Address upon which to listen. Set to
 #   0.0.0.0 to listen on all addresses.
 #
@@ -29,11 +38,14 @@
 #   allowed to connect to this service.
 #
 class rsync::server (
-  Boolean          $stunnel            = simplib::lookup('simp_options::stunnel', { default_value => true }),
-  Simplib::Port    $stunnel_port       = 8730,
-  Simplib::IP      $listen_address     = '0.0.0.0',
-  Boolean          $drop_rsyslog_noise = true,
-  Simplib::Netlist $trusted_nets       = simplib::lookup('simp_options::trusted_nets', { default_value => ['127.0.0.1'] })
+  Simplib::IP          $listen_address     = '0.0.0.0',
+  Boolean              $stunnel            = simplib::lookup('simp_options::stunnel', { default_value => true }),
+  Simplib::Port        $stunnel_port       = 8730,
+  Array[Simplib::Port] $stunnel_connect    = [873],
+  String               $stunnel_accept     = "${listen_address}:${stunnel_port}",
+  Boolean              $stunnel_client     = false,
+  Boolean              $drop_rsyslog_noise = true,
+  Simplib::Netlist     $trusted_nets       = simplib::lookup('simp_options::trusted_nets', { default_value => ['127.0.0.1'] })
 ) {
   include '::rsync'
   include '::rsync::server::global'
@@ -47,9 +59,9 @@ class rsync::server (
     include '::stunnel'
 
     stunnel::connection { 'rsync':
-      connect      => [873],
-      accept       => "${listen_address}:${stunnel_port}",
-      client       => false,
+      connect      => $stunnel_connect,
+      accept       => $stunnel_accept,
+      client       => $stunnel_client,
       trusted_nets => $trusted_nets
     }
   }
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -6,17 +6,26 @@ describe 'rsync::server' do
       let(:facts) { os_facts }
 
       context "on #{os}" do
-        it { is_expected.to compile.with_all_deps }
-
-        it { is_expected.to create_class('rsync') }
-        it { is_expected.to create_class('stunnel') }
-        it { is_expected.to create_concat('/etc/rsyncd.conf').that_notifies('Service[rsyncd]') }
-        it { is_expected.to create_service('rsyncd').that_subscribes_to('Service[stunnel]') }
+        context 'with default parameters' do
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_class('rsync') }
+          it { is_expected.to create_class('stunnel') }
+          it { is_expected.to create_stunnel__connection('rsync').with({
+            :connect      => [873],
+            :accept       => '0.0.0.0:8730',
+            :client       => false,
+            :trusted_nets => ['1.2.3.4','5.6.7.8']
+            })
+          }
+          it { is_expected.to create_concat('/etc/rsyncd.conf').that_notifies('Service[rsyncd]') }
+          it { is_expected.to create_service('rsyncd').that_subscribes_to('Service[stunnel]') }
+        end
 
         context 'no_stunnel' do
           let(:params){{ :stunnel => false }}
 
           it { is_expected.to compile.with_all_deps }
+          it { is_expected.to_not create_class('stunnel') }
           it { is_expected.to create_concat('/etc/rsyncd.conf').that_notifies('Service[rsyncd]') }
           it { is_expected.to create_service('rsyncd') }
           it { is_expected.to create_service('rsyncd').without_subscribes }
